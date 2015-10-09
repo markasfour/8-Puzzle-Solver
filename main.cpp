f3082ab6c16669c6ac1699d5215b32e4abef58b3
Mark Asfour <mj4sparrow@gmail.com>
2015-10-09 11:42:41 -0700

    added puzzle input

--- main.cpp
+++ main.cpp
@@ -3,5 +3,33 @@ using namespace std;
 
 int main()
 {
+	cout << "Welcome to Mark Asfour's 8-puzzle solver." << endl;
+	cout << "Type 1 to use a default puzzle, or 2 to enter your own puzzle." << endl;
+	int entry = 0;
+	while (entry != 1 && entry != 2)
+	{
+		cin >> entry;
+	}
+	int a, b, c, d, e, f, g, h, i;
+	if (entry == 1)
+	{
+		a = 1; b = 2; c = 3; d = 4;
+		e = 5; f = 6; g = 7; h = 8;
+		i = 0;
+	}
+	else
+	{
+		cout << "Enter your puzzle, use 0 to represent the blank" << endl;
+		cout << "Enter the first row, separate numbers by hitting enter" << endl;
+		cin >> a >> b >> c;
+		cout << "Enter the second row, separate numbers by hitting enter" << endl;
+		cin >> d >> e >> f;
+		cout << "Enter the third row, separate numbers by hitting enter" << endl;
+		cin >> g >> h >> i;
+	}
+	cout << a << b << c << endl;
+	cout << d << e << f << endl;
+	cout << g << h << i << endl;
+
 	return 0;
 }
