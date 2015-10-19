5f382747cca741978baa1938d1852d5146523978
Mark Asfour <mj4sparrow@gmail.com>
2015-10-18 17:10:29 -0700

    added heuristic functs

--- main.cpp
+++ main.cpp
@@ -59,6 +59,88 @@ vector <node> VISITED;
 //search queue
 vector <node> SEARCH;
 
+int misplaced_h(VECTOR x)
+{
+	int num = 0;
+	for(int i = 0; i < 3; i++)
+	{
+		for(int j = 0; j < 3; j++)
+		{
+			if(x.at(i).at(j) == 0) //skip the blank
+			{
+				continue;
+			}
+			if(x.at(i).at(j) != GOAL.at(i).at(j)) //check match
+			{
+				num++;
+			}
+		}
+	}
+	return num;
+}
+
+void find(int a, int &p, int &q)
+{
+	for(int i = 0; i < 3; i++)
+	{
+		for(int j = 0; j < 3; j++)
+		{
+			if(GOAL.at(i).at(j) == a)
+			{
+				p = i;
+				q = j;
+				return;
+			}
+		}
+	}
+}
+
+int manhattan_h(VECTOR x)
+{
+	int num = 0;
+	for(int i = 0; i < 3; i++)
+	{
+		for(int j = 0; j < 3; j++)
+		{
+			if(x.at(i).at(j) == 0) //skip space
+			{
+				continue;
+			}
+			if(x.at(i).at(j) != GOAL.at(i).at(j))
+			{
+				int a, b;
+				find(x.at(i).at(j), a, b);
+				a = a - i;
+				b = b - j;
+				while(a != 0 && b != 0)
+				{
+					if(a > 0)
+					{
+						a--;
+						num++;
+					}
+					if(a < 0)
+					{
+						a++;
+						num++;
+					}
+					if(b > 0)
+					{
+						b--;
+						num++;
+					}
+					if(b < 0)
+					{
+						b++;
+						num++;
+					}
+				}
+			}
+		}
+	}
+	return num;
+}
+
 bool already_visited (VECTOR x)
 {
 	for (int i = 0; i < VISITED.size(); i++)
