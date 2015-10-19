ae448acb636773b606ce8dc6ad9b659386a2d72d
Mark Asfour <mj4sparrow@gmail.com>
2015-10-18 17:33:05 -0700

    moved node code

--- main.cpp
+++ main.cpp
@@ -1,55 +1,10 @@
 #include <iostream>
 #include <vector>
 #include <unistd.h>
+#include "node.h"
 #include "operators.h"
 #include "heuristics.h"
 using namespace std;
-
-typedef vector < vector<int> > VECTOR;
-
-
-void PRINT(VECTOR x)
-{
-	for (int i = 0; i < 3; i++)
-	{
-		for (int j = 0; j < 3; j++)
-		{
-			cout << x.at(i).at(j);
-		}
-		cout << endl;
-	}
-}
-
-//node structure
-struct node
-{
-	VECTOR x;
-	int uniform_cost;
-	int heuristic_cost;
-	int total;
-	node *child1;
-	node *child2;
-	node *child3;
-	node *child4;
-	node *parent;
-
-	node(VECTOR a) //constructor
-	{
-		x = a;
-		uniform_cost = 0;
-		heuristic_cost = 0;
-	};
-	node(VECTOR a, int uc, int hc) //constructor
-	{
-		x = a;
-		uniform_cost = uc;
-		heuristic_cost = hc;
-	};
-	void print() //prints current puzzle
-	{
-		PRINT(x);	
-	}
-};
 
 //search tree
 vector <node> TREE; 
---
+++ node.h
@@ -0,0 +1,54 @@
+#ifndef NODE_H
+#define NODE_H
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+typedef vector < vector<int> > VECTOR;
+
+void PRINT(VECTOR x)
+{
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			cout << x.at(i).at(j);
+		}
+		cout << endl;
+	}
+}
+
+//node structure
+struct node
+{
+	VECTOR x;
+	int uniform_cost;
+	int heuristic_cost;
+	int total;
+	node *child1;
+	node *child2;
+	node *child3;
+	node *child4;
+	node *parent;
+
+	node(VECTOR a) //constructor
+	{
+		x = a;
+		uniform_cost = 0;
+		heuristic_cost = 0;
+	};
+	node(VECTOR a, int uc, int hc) //constructor
+	{
+		x = a;
+		uniform_cost = uc;
+		heuristic_cost = hc;
+	};
+	void print() //prints current puzzle
+	{
+		PRINT(x);	
+	}
+};
+
+
+#endif
